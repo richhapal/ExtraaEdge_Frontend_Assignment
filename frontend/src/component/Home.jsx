f358d5be1f56ffa2ff718ec21145f5c249c95c4e
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-04-01 23:15:50 +0530

    Implement Indicator

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,7 +1,44 @@
-import { Box, Heading } from "@chakra-ui/react";
-import React from "react";
-
+import { Box, Flex } from "@chakra-ui/react";
+import React, { useEffect, useRef } from "react";
+import CardLayout from "./CardLayout";
+import { useDispatch, useSelector } from "react-redux";
+import fetchUser from "../redux/api";
+import "./home.css";
+import { userActions } from "../redux/userSlice";
 const Home = () => {
-     return <Box></Box>;
+     const userList = useSelector((state) => state.userReducer.userList);
+     // console.log("UserLIst in component home", userList);
+     const dispatch = useDispatch();
+
+     useEffect(() => {
+          setTimeout(() => {
+               dispatch(fetchUser());
+          }, 1500);
+     }, []);
+
+     const handleDeleteUserByEmail = (email) => {
+          let updatedList = userList.filter((item) => item.email !== email);
+          dispatch(userActions.updateUserList(updatedList));
+     };
+
+     return (
+          <>
+               <Box>
+                    {!userList.length ? (
+                         <Box className="spinner">
+                              <div className="bounce1"></div>
+                              <div className="bounce2"></div>
+                              <div className="bounce3"></div>
+                         </Box>
+                    ) : (
+                         <Flex flexWrap="wrap">
+                              {userList.map((item) => (
+                                   <CardLayout key={item.email} {...item} handleDeleteUserByEmail={handleDeleteUserByEmail} />
+                              ))}
+                         </Flex>
+                    )}
+               </Box>
+          </>
+     );
 };
 export default Home;
